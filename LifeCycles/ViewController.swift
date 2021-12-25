6833d1c3d982d303914ea4fae2ab5b29b1b03663
umut9191 <umutsurmeli91@gmail.com>
2021-12-25 10:12:21 +0300

    Initial Commit

---
+++ LifeCycles/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LifeCycles
+//
+//  Created by Mac on 25.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
