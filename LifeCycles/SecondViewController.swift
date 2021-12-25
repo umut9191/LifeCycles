73e661a1d6738e086a46b43876c1d747933ca200
umut9191 <umutsurmeli91@gmail.com>
2021-12-25 11:13:29 +0300

    coded Lifecycles of Controllers,views status

--- LifeCycles/SecondViewController.swift
+++ LifeCycles/SecondViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SecondViewController.swift
 //  LifeCycles
 //
 //  Created by Mac on 25.12.2021.
@@ -7,29 +7,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("viewDidLoad")
+        print("Second viewDidLoad")
         
-        //be awera of viewDidLoad when passing around screens not invoking
-        // it is called when first time view loading
+        //be awera of because of this is second screen when we are going to this screen viewDidLoad always running
     }
     override func viewDidAppear(_ animated: Bool) {
-     print("viewDidAppear")
+     print("Second viewDidAppear")
     }
     override func viewDidDisappear(_ animated: Bool) {
-        print("viewDidDisappear")
+        print("Second viewDidDisappear")
     }
     override func viewWillDisappear(_ animated: Bool) {
-        print("viewWillDisappear")
+        print("Second viewWillDisappear")
     }
     override func viewWillAppear(_ animated: Bool) {
-        print("viewWillAppear")
+        print("Second viewWillAppear")
     }
-
-
 }
-
